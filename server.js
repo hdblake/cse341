50f63ebca420e51b58ea883fb15d3e0beab59257
= <=>
2023-10-05 13:13:23 -0500

    updated app

--- server.js
+++ server.js
@@ -5,26 +5,25 @@ const cors = require("cors");
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.json()).use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://cse341-o9h4.onrender.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
-  );
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, PUT, POST, DELETE, OPTIONS"
-  );
-  next();
-});
-
-app.use(cors());
-
-app.use("/", require("./routes"));
+app
+  .use(express.json())
+  .use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://cse341-o9h4.onrender.com"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, PUT, POST, DELETE, OPTIONS"
+    );
+    next();
+  })
+  .use("/", require("./routes"));
 
 mongodb.initDb((error, mongodb) => {
   if (error) {
